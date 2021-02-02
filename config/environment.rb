99b4d169ea7d306436551c06c23d8a61ce8f5a88
nick <nicholas_scalione@yahoo.com>
2021-02-02 14:52:32 -0500

    set up folders/environment folder/and gemfile

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require_relative('../lib/api')
+require_relative('../lib/cli')
+require_relative('../lib/nba_players')
+require_relative('../lib/nba_teams')
+require_relative('../lib/players_stats')
+
+require 'pry'
+
