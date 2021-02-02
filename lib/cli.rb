ec8bade82194aaf913be48ee4b73c82d7950fd03
nick <nicholas_scalione@yahoo.com>
2021-02-02 15:03:02 -0500

    defines welcome method and first user input method, first test run through bin/run

---
+++ lib/cli.rb
@@ -0,0 +1,37 @@
+class Cli  
+
+
+    def welcome
+        puts "Hello, Welcome! Let's go!"
+        self.prompt_selection
+    end
+
+    def prompt_selection
+        puts "Please select a number"
+        puts "1. Teams"
+        puts "2. Players"
+        puts "3. Stats"
+        input = gets.strip
+        if input == "1"
+            self.teams  
+        elsif input == "2"
+            self.players  
+        elsif input == "3"
+            self.stats  
+        else
+            puts "Hey Wise-Guy! I says pick a numbah 1 through 3!"
+            self.prompt_selection
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+
+end
