8d20540abec912d94fd3a50fa062687cde69fe0d
nick <nicholas_scalione@yahoo.com>
2021-02-03 22:56:48 -0500

    Set up get_player method in Api Class to reflect wanted player info in hash

--- lib/api.rb
+++ lib/api.rb
@@ -24,18 +24,18 @@ class Api
 
    def self.get_player
     response = HTTParty.get(@@url2)
-    response.collect do |attribute, value|
-        puts ["#{attribute}: #{value}"]
+    response["data"].collect do |name|
+        player_hash = {first_name: name["first_name"], last_name: name["last_name"], position: name["position"], height: name["height_feet ' height_inches ''"], weight_pounds: name["weight_pounds"], team: name["team"]["full_name"]}
+        binding.pry
         Players.new(response)
     end
 end
-
 @@url3 = "https://www.balldontlie.io/api/v1/season_averages"
 
 def self.get_stats
     response = HTTParty.get(@@url3)
-    response.collect do |attribute, value|
-        puts "#{attribute}: #{value}"
+    response.collect do |attribute, team|
+        puts "#{attribute}: #{team}"
         
         Stats.new(response)
     end
