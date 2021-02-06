adff702845d693e9d595ad7b563be73f102c1294
nick <nicholas_scalione@yahoo.com>
2021-02-05 19:17:24 -0500

    added a salary file/class to initialize player salaries and add it to player/team info

---
+++ lib/salary.rb
@@ -0,0 +1,32 @@
+class Salary
+
+    @@all = []
+        
+
+    def initialize(salary_hash)
+            salary_hash.each do |key, value|
+            self.class.attr_accessor(key)
+            self.send(("#{key}="), value)
+            @@all << self unless @@all.include? self
+        end
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
